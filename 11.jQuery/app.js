95e76ec1560e1fc46706aa52e2a179d210acba6a
fpotapo <opensourcedevil@hotmail.com>
2015-11-01 21:44:31 +0800

    2

---
+++ 11.jQuery/app.js
@@ -0,0 +1,83 @@
+//$(document).ready(function(){
+    //alert("dd");
+    //$("p").click(function() {
+    //    $(this).hide();
+    //});
+    //$("button").mouseenter( function() {
+        //$("p").hide();
+        //$("p").text("ddd");
+        //$(this).hide();
+    //});
+//});
+//$(document).ready(function() {
+//    $("button").bind("click", clickHeader1);
+//    $("button").bind("click", clickHeader2);
+//
+//});
+//function clickHeader1() {
+//    console.log("clickHeader1");
+//}
+//function clickHeader2() {
+//    console.log("clickHeader2");
+//}
+//$(document).ready(function() {
+//    $("body").click(function(event) {
+//        console.log(event);
+//    });
+//    $("div").click(function(event) {
+//       console.log(event);
+//        event.stopPropagation();
+//    });
+//});
+//var clickMeBtn;
+//$(document).ready(function() {
+//    clickMeBtn = $("#clickMeBtn");
+//    clickMeBtn.click(function() {
+//        var e = jQuery.Event("MyEvent");
+//        clickMeBtn.trigger(e);
+//    });
+//    clickMeBtn.bind("MyEvent", function(event) {
+//       console.log(event);
+//    });
+//
+//
+//});
+//$(document).ready(function() {
+    //$("button").click(function() {
+    //    $("p").text(function(i, ot) {
+    //        return "ot"+ot +(i);
+    //    });
+    //});
+    //$("button").click(function(){
+    //    $("p").before("ddd");
+    //});
+
+    //function appendText() {
+    //    var text1 = "<p>pp1</p>";
+    //    var text2 = $("<p></p>").text("pp2");
+    //    var text3 = document.createElement("p");
+    //    text3.appendChild(document.createTextNode("pp3"));
+    //    $("body").append(text1, text2, text3);
+    //};
+    //$("button").click(function() {
+    //    appendText();
+    //});
+//});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
